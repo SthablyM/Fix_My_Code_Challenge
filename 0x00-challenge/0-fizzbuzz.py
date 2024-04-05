19db0f1312f4f29fa25837cac869f65934e3f6e0
SthablyM <mathapelofentse@icloud.com>
2024-04-05 10:32:07 -0700

    fizzbuzzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -19,14 +19,14 @@ def fizzbuzz(n):
 
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
-            temp_result.append("FizzBuzz ")
+            temp_result.append("FizzBuzz")
         elif (i % 3) == 0:
-            temp_result.append("Fizz ")
+            temp_result.append("Fizz")
         elif (i % 5) == 0:
-            temp_result.append("Buzz ")
+            temp_result.append("Buzz")
         else:
-            temp_result.append(str(i)+" ")
-    print("".join(temp_result))
+            temp_result.append(str(i))
+    print(" ".join(temp_result))
 
 
 
